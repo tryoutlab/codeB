447505476d5ad980204a148c86e87e71b162e93c
tryoutlab <tryoutlab@gmail.com>
2019-11-20 17:57:19 +0900

    uiKitが動いた

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,13 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import UIkit from 'uikit/dist/js/uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
+import './stylesheets/application'
+
+// loads the Icon plugin
+UIkit.use(Icons);
+
+// components can be called from the imported UIkit reference
+UIkit.notification('Hello world.');
