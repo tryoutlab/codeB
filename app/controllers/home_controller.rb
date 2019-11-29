4c7579ae76b4d407a506d2e068a90e794c48b47e
tryoutlab <tryoutlab@gmail.com>
2019-11-30 02:59:46 +0900

    popen3周りを追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
+  
   def index
     @repo = Repo.new
   end
@@ -7,6 +8,7 @@ class HomeController < ApplicationController
     @repo = Repo.new(repo_params)
     if @repo.valid?
       # 成功した場合
+      download_repo(repo_params[:repourl])
       render 'home/index'
     else
       # 失敗した場合
@@ -19,4 +21,12 @@ class HomeController < ApplicationController
     params.require(:repo).permit(:repourl)
   end
 
+  def download_repo(repo_url)
+    Open3.popen3("wget #{repo_url}") do |i, o, e, w|
+      
+      # コマンド結果はstderr(エラー出力)に返される
+      e.each { |line| puts line }
+
+    end
+  end
 end
