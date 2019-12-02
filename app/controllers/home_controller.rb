0095a4699ea4b76780111b3eec01ee502769c600
tryoutlab <tryoutlab@gmail.com>
2019-12-03 02:32:32 +0900

    コマンド調整。Rubocop、onkcop導入

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,8 +8,8 @@ class HomeController < ApplicationController
     @repo = Repo.new(repo_params)
     if @repo.valid?
       # 成功した場合
-      @cmd_result = download_repo(repo_params[:repourl])
-      puts @cmd_result
+      @gitclone_result = gitclone_repo(repo_params[:repourl])
+      #puts @gitclone_result
       render 'home/check'
     else
       # 失敗した場合
@@ -22,9 +22,14 @@ class HomeController < ApplicationController
     params.require(:repo).permit(:repourl)
   end
 
-  def download_repo(repo_url)
-    o, e, s = Open3.capture3("wget #{repo_url}")
-    e
+  def gitclone_repo(repo_url)
+    #o, e, s = Open3.capture3("cd /tmp && ls && git clone #{repo_url}")
+    repo_url_split = repo_url.split("/")
+    p repo_url_split
+    repo_name = repo_url_split[4].split(".").slice(0)
+    o, e, s = Open3.capture3("git clone --progress --verbose #{repo_url} /tmp/#{repo_name} 2>&1")
+
+    o
     #Open3.popen3("wget #{repo_url}") do |i, o, e, w|
       
       # コマンド結果はstderr(エラー出力)に返される
